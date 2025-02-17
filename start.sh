d59289e5cbe3a1274ac23be5a72334ab17d50dd6
zzxym <109533728+zzxym@users.noreply.github.com>
2025-02-17 09:33:22 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -25,4 +25,6 @@ cp docker-compose.yaml docker-compose.yaml.bak
 
 sed -i "s/127.0.0.1/$external_ip/g" docker-compose.yaml
 
-docker compose -p openvpn-master up -d --build
+sed "s/127.0.0.1/$external_ip/g" docker-compose-slave.yaml
+
+docker-compose -p openvpn-master up -d --build
